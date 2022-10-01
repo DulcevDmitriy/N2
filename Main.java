98ffea2442ce1275c98ba0b184703450692fbba5
Dulcev Dmitriy <ddvvvvvv@yandex.ru>
2022-10-01 21:16:46 +0300

    Initial commit

--- Main.java
+++ Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 11000
+        int price = 11000;
 
-        int bonusMiles = price:20;
+        int bonusMiles = price / 20;
         System.out.println("Бонусные мили: " + bonusMiles);
-
 
     }
 }
